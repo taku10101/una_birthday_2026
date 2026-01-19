012a5ece6921e8717f9de6cf439c3ee390616107
taku10101 <taku1011ht@gmail.com>
2026-01-20 01:22:10 +0900

    update: slider data

--- src/data/slides.ts
+++ src/data/slides.ts
@@ -7,9 +7,34 @@ export interface Slide {
 
 export const slides: Slide[] = [
   {
-    title: "スライド1",
+    title: "たくと",
     titleColor: "#ff69b4",
-    icon: "/icon.png",
-    content: "/slides/slide1.md"
+    icon: "/tak.png",
+    content: "/slides/tak.md"
   },
+  {
+    title:"薄切り",
+    titleColor: "#ff69b4",
+    icon: "/usugiri.png",
+    content: "/slides/usugiri.md"
+  },
+  {
+    title: "まめ",
+    titleColor: "#ff69b4",
+    icon: "/mame.png",
+    content: "/slides/mame.md"
+  },
+  {
+    title: "しらす",
+    titleColor: "#ff69b4",
+    icon: "/sirasu.png",
+    content: "/slides/sirasu.md"
+  },
+  {
+    title: "88",
+    titleColor: "#ff69b4",
+    icon: "/88.png",
+    content: "/slides/88.md"
+  },
+
 ]
