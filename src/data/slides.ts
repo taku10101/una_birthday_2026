878179279c75a208fa8bce394cacbc157cef6543
taku10101 <taku1011ht@gmail.com>
2026-01-21 20:19:33 +0900

    fix html

--- src/data/slides.ts
+++ src/data/slides.ts
@@ -10,25 +10,25 @@ export const slides: Slide[] = [
     title: "たくと",
     titleColor: "#ff69b4",
     icon: "/tak.png",
-    content: "/slides/tak.md"
+    content: "/slides/tak.html"
   },
   {
     title:"薄切り",
     titleColor: "#ff69b4",
     icon: "/usugiri.png",
-    content: "/slides/usugiri.md"
+    content: "/slides/usugiri.html"
   },
   {
     title: "まめ",
     titleColor: "#ff69b4",
     icon: "/mame.png",
-    content: "/slides/mame.md"
+    content: "/slides/mame.html"
   },
   {
     title: "しらす",
     titleColor: "#ff69b4",
     icon: "/sirasu.png",
-    content: "/slides/sirasu.md"
+    content: "/slides/sirasu.html"
   },
   {
     title: "88",
